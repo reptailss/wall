1ebc649341e6101e041683c3995048a27b54b939
reptailss <reptailss@gmail.com>
2022-11-16 21:58:25 +0200

    'fix'

--- src/containers/search/searchInput/SearchInput.tsx
+++ src/containers/search/searchInput/SearchInput.tsx
@@ -2,24 +2,19 @@ import React, {FC, useState} from 'react';
 
 import styles from './styles.module.scss'
 import TextField from '@mui/material/TextField';
-import {Button, Paper} from "@mui/material";
 
-
-import SendIcon from '@mui/icons-material/Send';
-import { v4 as uuidv4 } from 'uuid';
-import {setWhereSearchValue} from '../../../redux/slice/userSlice'
 import {useAppDispatch, useAppSelector} from "../../../hooks/redux";
 
 const SearchInput = () => {
 
     const [value, setValue] = useState('');
 
-    const{searchValue} = useAppSelector(state => state.user)
+
 
     const dispatch = useAppDispatch();
 
     const onchangeValue = (value: string) => {
-        dispatch(setWhereSearchValue({value}))
+
 
     };
 
@@ -37,7 +32,6 @@ const SearchInput = () => {
                     id={'search'}
                     name={'search'}
                     label={'пошук..'}
-                    value={searchValue}
                     onChange={(event) => onchangeValue(event.target.value)}
                 />
             </div>
